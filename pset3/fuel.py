70bea7f582149b714a3860e9e90604d6b84e16b7
wh1l34tw0rk-spec <wh1l3.4t.w0rk@gmail.com>
2025-09-09 12:39:23 -0500

    Create fuel.py

---
+++ pset3/fuel.py
@@ -0,0 +1,17 @@
+def check_fuel(amount):
+    # Split string at "/" + Convert to int
+    # Convert fraction to input
+    #
+    #
+    return amount
+
+
+def main():
+    print("Can't read fractions? Enter your fuel value and get back a percentage")
+    fuel_level = input("Fraction: ")
+    fuel = check_fuel(fuel_level)
+    print(fuel)
+
+if __name__ == "__main__":
+    main()
+
